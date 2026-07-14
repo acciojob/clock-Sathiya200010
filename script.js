5018cbb42fd1af9d621560a105de02e5754eb2a6
Sathiya R <rskeerthi01@gmail.com>
2026-07-14 22:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+//your JS code here. If requi
+
+function updateTime() {
+    const timer = document.getElementById("timer");
+    timer.textContent = new Date().toLocaleTimeString();
+}
+
+// Update immediately
+updateTime();
+
+// Update every second
+setInterval(updateTime, 1000);
